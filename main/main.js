43dda26c5cae8a1f03a50df5706f05f217acfdb3
cuiliyang <1036612821@qq.com>
2019-05-29 21:38:58 +0800

    add JS02-LCD

---
+++ main/main.js
@@ -0,0 +1,17 @@
+//LCD
+const numbers = [
+    ['._.','...','._.','._.','...','._.','._.','._.','._.','._.'],
+    ['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'],
+    ['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|']
+]
+//输入参数str
+module.exports = function main(str) {
+    const result = [[], [], []]
+    //s为str中的数 对应下边
+    for(let s of str){
+        result[0].push(numbers[0][s])
+        result[1].push(numbers[1][s])
+        result[2].push(numbers[2][s])
+    }
+    return result.map(row => row.join(' ')+'\n').join('')
+};
